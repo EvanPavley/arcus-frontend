19d69e79041468e9590aaf8c51c3cffeff49c0a2
EvanPavley <github email address>
2019-04-04 01:31:44 -0400

    found a working breakpoint

--- src/components/js/Navbar.js
+++ src/components/js/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setCurrentUser } from '../../redux/actions';
-
+import MediaQuery from 'react-responsive';
 
 import logo from '../../images/Arcuslogo.png'
 import '../css/Navbar.css';
@@ -14,39 +14,79 @@ const Navbar = (props) => {
   }
 
   return (
-    <div className='nav-container'>
-      <div className='cool-nav-container'>
-        <input
-          className='image'
-          type='image'
-          alt='logo'
-          src={logo}
-          onClick={() => props.history.push('/HomePage')}
-        />
-        <NavLink className='nav-item' to='/ColorPalletGenerator'>
-          Color Palette Generator{' '}
-        </NavLink>
-        <NavLink className='nav-item' to='/Profile'>
-          Your Palettes{' '}
-        </NavLink>
-      </div>
-
-      {props.current_user === null ? (
-        <div className='log-nav-container'>
-          <NavLink className='nav-item' to='/Login'>
-            Login{' '}
-          </NavLink>
-          <NavLink id="signup" to='/Signup'>
-            Sign Up{' '}
-          </NavLink>
-        </div>
-      ) : (
-        <div className='log-nav-container' onClick={handleLogout}>
-          <div id="signup">
-            Logout
+    <div>
+      <MediaQuery orientation="portrait">
+        <div className='nav-container'>
+          <div className='cool-nav-container'>
+            <input
+              className='image'
+              type='image'
+              alt='logo'
+              src={logo}
+              onClick={() => props.history.push('/HomePage')}
+            />
+            <NavLink className='nav-item' to='/ColorPalletGenerator'>
+              Color Palette Generator{' '}
+            </NavLink>
+            <NavLink className='nav-item' to='/Profile'>
+              Your Palettes{' '}
+            </NavLink>
           </div>
+
+          {props.current_user === null ? (
+            <div className='log-nav-container'>
+              <NavLink className='nav-item' to='/Login'>
+                Login{' '}
+              </NavLink>
+              <NavLink id="signup" to='/Signup'>
+                Sign Up{' '}
+              </NavLink>
+            </div>
+          ) : (
+            <div className='log-nav-container' onClick={handleLogout}>
+              <div id="signup">
+                Logout
+              </div>
+            </div>
+          )}
         </div>
-      )}
+      </MediaQuery>
+      <MediaQuery orientation="landscape">
+        <div className='nav-container'>
+          <div className='cool-nav-container'>
+            <input
+              className='image'
+              type='image'
+              alt='logo'
+              src={logo}
+              onClick={() => props.history.push('/HomePage')}
+            />
+            <NavLink className='nav-item' to='/ColorPalletGenerator'>
+              Color Palette Generator{' '}
+            </NavLink>
+            <NavLink className='nav-item' to='/Profile'>
+              Your Palettes{' '}
+            </NavLink>
+          </div>
+
+          {props.current_user === null ? (
+            <div className='log-nav-container'>
+              <NavLink className='nav-item' to='/Login'>
+                Login{' '}
+              </NavLink>
+              <NavLink id="signup" to='/Signup'>
+                Sign Up{' '}
+              </NavLink>
+            </div>
+          ) : (
+            <div className='log-nav-container' onClick={handleLogout}>
+              <div id="signup">
+                Logout
+              </div>
+            </div>
+          )}
+        </div>
+      </MediaQuery>
     </div>
   )
 }
