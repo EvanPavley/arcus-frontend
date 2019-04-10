cc7a9bbbde8a5cb037534161dcda78a65103e48c
EvanPavley <epavley@sva.edu>
2019-04-10 16:14:03 -0400

    debugging heroku

--- src/configURL.js
+++ src/configURL.js
@@ -1,8 +1,5 @@
 let API_URL
-if (process.env["NODE_ENV"] === "development") {
-  API_URL = "http://localhost:3001"
-}else{
-  API_URL = "https://arcus-color-palette-api.herokuapp.com"
-}
+
+API_URL = "https://arcus-color-palette-api.herokuapp.com"
 
 export default API_URL
